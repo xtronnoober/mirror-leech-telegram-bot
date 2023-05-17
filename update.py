2df0ce7f07e8f58077c81ed6e5b190c7be6e40f8
xtronnoober <85035009+xtronnoober@users.noreply.github.com>
2023-05-18 00:39:03 +0530

    Update update.py

--- update.py
+++ update.py
@@ -51,16 +51,16 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = 'https://github.com/xtronnoober/mirror-leech-telegram-bot' 
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'master'
+    UPSTREAM_BRANCH = 'render'
 
-if UPSTREAM_REPO is not None:
+if UPSTREAM_REPO := 'https://github.com/xtronnoober/mirror-leech-telegram-bot':
     if ospath.exists('.git'):
         srun(["rm", "-rf", ".git"])
-
+        
     update = srun([f"git init -q \
                      && git config --global user.email e.anastayyar@gmail.com \
                      && git config --global user.name mltb \
